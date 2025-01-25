c30112bc29eb2c633afb5a57aaffeb8f4b80f20a
pratyush majumdar <pratyushmajumdar27@gmail.com>
2025-01-25 18:53:43 +0530

    workflow done

--- functions.py
+++ functions.py
@@ -2,11 +2,21 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.utilities import SQLDatabase
 from langchain_core.output_parsers import StrOutputParser  # for parsing the output into a string
 from langchain_core.runnables import RunnablePassthrough  # allows the function to pass as a runnable
+from langchain_groq import ChatGroq
+from secret_key import sec_key
+
+model_name = "llama-3.1-8b-instant"  # name of model used
+llm = ChatGroq(
+    model_name=model_name,
+    temperature=0.1,  # more accurate results
+    groq_api_key=sec_key
+)
 
 
-def get_sql_chain(db: SQLDatabase, llm):
+def get_sql_chain(db: SQLDatabase):  # function to get sql query
     template = """
-        Based on the table schema below, write only a SQL query that would answer the user's question:
+        Based on the table schema below, write only a SQL query that would answer the user's question.
+        Don't provide any extra information other than the sql query.
         {schema}
         Conversation History: {chat_history}
 
@@ -15,7 +25,7 @@ def get_sql_chain(db: SQLDatabase, llm):
     """
     prompt = ChatPromptTemplate.from_template(template)
 
-    def get_schema(_):
+    def get_schema(_):  # function to get schema info
         return db.get_table_info()
 
     return (
@@ -24,3 +34,33 @@ def get_sql_chain(db: SQLDatabase, llm):
         | llm.bind(stop=["\nSQLResult:"])
         | StrOutputParser()
     )
+
+
+def get_response(user_query: str, db: SQLDatabase, chat_history: list):  # function to display natural language response output
+    sql_chain = get_sql_chain(db)
+    template = """
+       Based on the table schema below, question, SQL query, and SQL response, write only a natural language response to the user's question.
+       {schema}
+       Conversation History: {chat_history}
+       
+       Question: {question}
+       SQL Query: {query}
+       SQL Response: {response}
+       Natural Language Response:
+    """
+    prompt = ChatPromptTemplate.from_template(template)  # another template to get the natural language response
+
+    chain = (
+        RunnablePassthrough.assign(query=sql_chain).assign(
+            schema=lambda _: db.get_table_info(),
+            response=lambda var: db.run(var["query"]),
+        )
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    return chain.invoke({
+        "question": user_query,
+        "chat_history": chat_history,
+    })
--- main.py
+++ main.py
@@ -2,82 +2,47 @@ import os
 from secret_key import sec_key
 import streamlit as st
 from langchain_community.utilities import SQLDatabase
-from langchain_groq import ChatGroq
 from langchain_core.messages import AIMessage, HumanMessage
-from functions import get_sql_chain
+from functions import get_response
 
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = sec_key  # set environment variable
-model_name = "llama-3.1-8b-instant"  # name of model used
-llm = ChatGroq(
-    model_name=model_name,
-    temperature=0.1,  # more accurate results
-    groq_api_key=sec_key
-)
+os.environ['GROQ_API_KEY'] = sec_key  # set environment variable
 
-if "db" not in st.session_state:
+if "db" not in st.session_state:  # initialize database
     mysql_uri = "mysql+mysqlconnector://root_readonly:matsumoto@localhost:3307/property_tax"
     st.session_state.db = SQLDatabase.from_uri(mysql_uri)
 
-if "chat_history" not in st.session_state:
+if "chat_history" not in st.session_state:  # initialize chat_history
     st.session_state.chat_history = [
         AIMessage(content="Hello, I'm a SQL assistant, Ask me anything about your database")
     ]
 
-# def get_response(user_query: str, db: SQLDatabase, chat_history: list):
-#     sql_chain = get_sql_chain(db)
-#     template = """
-#        You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
-#        Based on the table schema below, question, SQL query, and SQL response, write only a natural language response to the user's question.
-#        <SCHEMA>{schema}</SCHEMA>
-#
-#        Conversation History: {chat_history}
-#        SQL Query: <SQL>{query}</SQL>
-#        User question: {question}
-#        SQL Response: {response}"""
-#     prompt = ChatPromptTemplate.from_template(template)  # another template to get the natural language response
-#
-#     chain = (
-#         RunnablePassthrough.assign(query=sql_chain).assign(
-#             schema=lambda _: db.get_table_info(),
-#             response=lambda var: db.run(var["query"]),
-#         )
-#         | prompt
-#         | llm
-#         | StrOutputParser()
-#     )
-#
-#     return chain.invoke({
-#         "question": user_query,
-#         "chat_history": chat_history,
-#     })
 
-
-st.set_page_config(
+st.set_page_config(  # page configuration
     page_title="Chat with MySQL",
     page_icon=":speech_balloon:"
 )
 
-st.title("MySQL Database Q&A Tool")
+st.title("MySQL Database Q&A Tool")  # gives title
 for message in st.session_state.chat_history:
-    if isinstance(message, AIMessage):
+    if isinstance(message, AIMessage):  # for AIMessage
         with st.chat_message("AI"):
             st.markdown(message.content)
-    elif isinstance(message, HumanMessage):
+    elif isinstance(message, HumanMessage):  # for HumanMessage
         with st.chat_message("Human"):
             st.markdown(message.content)
 
-user_query = st.chat_input("Type a message...")
+user_query = st.chat_input("Type a message...")  # asks user for prompt
 if user_query is not None and user_query.strip() != "":
-    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    st.session_state.chat_history.append(HumanMessage(content=user_query))  # update chat_history
     with st.chat_message("Human"):
-        st.markdown(user_query)
+        st.markdown(user_query)  # display human message
 
     with st.chat_message("AI"):
-        sql_chain = get_sql_chain(st.session_state.db, llm=llm)
-        # response = get_response(user_query, st.session_state.db, st.session_state.chat_history)
-        response = sql_chain.invoke({
-            "chat_history": st.session_state.chat_history,
-            "question": user_query
-        })
-        st.markdown(response)
+        # sql_chain = get_sql_chain(st.session_state.db)  # for query testing
+        response = get_response(user_query, st.session_state.db, st.session_state.chat_history)
+        # response = sql_chain.invoke({  # for query testing
+        #     "chat_history": st.session_state.chat_history,
+        #     "question": user_query
+        # })
+        st.markdown(response)  # display ai message
     st.session_state.chat_history.append(AIMessage(content=response))
